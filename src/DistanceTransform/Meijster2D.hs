2067307ae8ffb00b6510411d3ab792097f5e774a
Anthony Cowley <acowley@gmail.com>
2012-11-11 02:28:40 -0500

    Some code sanitization.

--- src/DistanceTransform/Meijster2D.hs
+++ src/DistanceTransform/Meijster2D.hs
@@ -2,13 +2,10 @@
 module DistanceTransform.Meijster2D (edt) where
 import Control.Applicative
 import Control.Monad (when, foldM)
-import Control.Monad.ST (ST)
+import Control.Monad.Primitive (PrimMonad, PrimState)
 import Data.Vector.Storable (Vector, (!))
 import qualified Data.Vector.Storable as V
 import qualified Data.Vector.Storable.Mutable as VM
-import DistanceTransform.Internal.Extended
-
-import Debug.Trace
 
 -- Function @f@ will be applied sequentially along each row using the
 -- given column indices.
@@ -22,13 +19,16 @@ colsM :: Monad m => Int -> Int -> [Int] -> (Int -> m ()) -> m ()
 colsM m n rs f = mapM_ aux [0..m-1]
   where aux c = mapM_ f $ map ((c +) . (m*)) rs -- [c,c+m .. m*(n-1)+c]
 
+newRW :: PrimMonad m => Int -> 
+         m (VM.MVector (PrimState m) Int, Int -> m Int, Int -> Int -> m ())
+newRW n = do { v <- VM.new n; return (v, VM.unsafeRead v, VM.unsafeWrite v) }
+
 -- Build Meijster's G function that finds nearest neighbors down and
--- up columns.
+-- up columns. This is phase 1 of the distance transform.
+prepareG :: Int -> Int -> Vector Int -> Vector Int
 prepareG m n p = V.create $ 
-                 do v <- VM.new (m*n)
-                    let vwrite = VM.unsafeWrite v
-                        vread = VM.unsafeRead v
-                        pullDown i = if p ! i == 0
+                 do (v, vread, vwrite) <- newRW (m*n)
+                    let pullDown i = if p ! i == 0
                                      then vwrite i 0
                                      else vread (i-m) >>= vwrite i . (1+)
                         pushUp i = do below <- vread (i+m)
@@ -55,28 +55,22 @@ data Temps m = Temps { swrite  :: Int -> Int -> m ()
 
 -- Initialize temporary arrays and partially applied per-row functions
 -- f and sep.
-prepTemps :: Int -> (Int -> Int) -> ST s (Temps (ST s))
-prepTemps m gsq = do s <- VM.new m
-                     t <- VM.new m
-                     VM.unsafeWrite s 0 0
-                     VM.unsafeWrite t 0 0
-                     return $ Temps (VM.unsafeWrite s) (VM.unsafeRead s)
-                                    (VM.unsafeWrite t) (VM.unsafeRead t)
-                                    f sep
+prepTemps :: PrimMonad m => Int -> (Int -> Int) -> m (Temps m)
+prepTemps m gsq = do (_, sread, swrite) <- newRW m
+                     (_, tread, twrite) <- newRW m
+                     swrite 0 0
+                     twrite 0 0
+                     return $ Temps swrite sread twrite tread f sep
   where f x i = let d = x - i in d*d + gsq i
         sep i u = (u*u - i*i + gsq u - gsq i) `quot` 2*(u-i)
 
-traceMsg :: Show a => String -> a -> a
-traceMsg msg x = trace (msg++": "++show x) x
-
 -- Phase 2 forward scan across a row.
-scan3 :: Monad m => Int -> Temps m -> Int -> Int -> m Int
+scan3 :: (Applicative m, Monad m) => Int -> Temps m -> Int -> Int -> m Int
 scan3 m Temps{..} !q0 !u =
   do q <- iterateWhileM qaux q0
      if q < 0
      then swrite 0 u >> return 0
-     else do sq <- sread q
-             let w = 1 + sep sq u
+     else do !w <- (1+) <$> (sep <$> sread q <*> pure u)
              if w < m
              then let !q' = q+1
                   in do swrite q' u
@@ -102,96 +96,18 @@ scan4 rowOffset dtwrite Temps{..} !q !u =
 
 phase2 :: Int -> Int -> (Int -> Int -> Int) -> Vector Int
 phase2 m n gRow = V.create $
-                  do --dt <- VM.new (m*n)
-                     dt <- VM.replicate (m*n) 0
+                  do dt <- VM.new (m*n)
                      mapM_ (aux (VM.unsafeWrite dt)) [0..n-1]
                      return dt
   where aux dt y = do temps <- prepTemps m (gRow y)
                       q <- foldM (scan3 m temps) 0 [1..m-1]
                       foldM (scan4 (y*m) dt temps) q [m-1,m-2..0]
                       return ()
-        
-sedt :: (Ord a, Num a, VM.Storable a) => 
-        Int -> Int -> Vector a -> Vector Int
+
+sedt :: Int -> Int -> Vector Int -> Vector Int
 sedt m n p = phase2 m n gRow
   where gSq = V.map (\x->x*x) $ prepareG m n p
         gRow r = (V.slice (r*m) m gSq !)
-
-sedtBusted :: (Ord a, Num a, VM.Storable a) => 
-        Int -> Int -> Vector a -> Vector Int
-sedtBusted m n p = f
-  where gRow :: Int -> Int -> Int
-        gRow r = let g' = V.map ((\x -> x*x) . unextend (n+m)) g
-                 in (V.slice (r*m) m g' !)
-        g,gt :: Vector (Extended Int)
-        gt = V.create $ do v <- VM.new (m*n)
-                           VM.set (VM.slice 0 m v) PosInf
-                           let go !i
-                                 | i >= m*n = return v
-                                 | p ! i == 0 = VM.unsafeWrite v i 0 >> go (i+1)
-                                 | otherwise = VM.unsafeRead v (i-m) >>=
-                                               VM.unsafeWrite v i . (1+) >>
-                                               go (i+1)
-                           go m
-        g = V.create $ do v <- VM.new (m*n)
-                          V.unsafeThaw (V.slice (m*n-m) m gt) >>=
-                            VM.unsafeCopy (VM.slice (m*n-m) m v)
-                          let go !i
-                                | i < 0 = return v
-                                | otherwise = (1+) <$> VM.unsafeRead v (i+m) >>=
-                                              VM.unsafeWrite v i . min (gt ! i)
-                                              >> go (i-1)
-                          go (m*n-m-1)
-        f = V.create $ 
-            do s <- VM.new m
-               t <- VM.new m
-               dt <- VM.new (m*n)
-               let rowInit = VM.unsafeWrite s 0 0 >> VM.unsafeWrite t 0 0
-               let v !! i = VM.unsafeRead v i
-                   phase2 !y
-                     | y == n = return dt
-                     | otherwise = 
-                       let g' = gRow y
-                           f x i = if i >= m
-                                   then error ("koni "++show i)
-                                   else let d = x - i in g' i + d*d
-                           ff q x i = if i >= m
-                                      then error ("biden "++show i ++"; q = "++show q++"; x = "++show x)
-                                      else let d = x - i in g' i + d*d
-                           sep i u = let gu = g' u
-                                         gi = g' i
-                                     in u*u-i*i+gu*gu-gi*gi `quot` 2*(u-i)
-                           scan3 !u !q
-                             | u == m = return q
-                             | otherwise = 
-                               let gu = g' u
-                                   qLoop q'
-                                     | q' < 0 = return q'
-                                     | otherwise = do tq <- t !! q'
-                                                      sq <- s !! q'
-                                                      if ff q' tq sq > let d = tq - u
-                                                                       in gu + d*d
-                                                        then qLoop (q' - 1)
-                                                        else return q'
-                               in do q' <- qLoop q
-                                     if q' < 0 
-                                       then do VM.unsafeWrite s 0 u
-                                               scan3 (u+1) 0
-                                       else do sq <- s !! q'
-                                               let w = 1 + sep sq u
-                                               when (w < m)
-                                                    (VM.unsafeWrite s q' u >>
-                                                     VM.unsafeWrite t q' w)
-                                               scan3 (u+1) (q'+1)
-                           scan4 !u !q
-                             | u < 0 = return ()
-                             | otherwise = do f <$> pure u <*> (s !! q) >>=
-                                                VM.unsafeWrite dt (y*m+u) 
-                                              tq <- t !! q
-                                              if tq == u then scan4 (u-1) (q-1)
-                                                         else scan4 (u-1) q
-                       in rowInit >> scan3 1 0 >>= scan4 (m-1) >> phase2 (y+1)
-               phase2 0
 
 edt :: Int -> Int -> Vector Int -> Vector Float
 edt w h v = V.map aux $ sedt w h v
