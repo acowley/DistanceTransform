6b0b0b8dca114a6ca7e8f4d8e03da455eaa4cfb6
Anthony Cowley <acowley@gmail.com>
2012-11-11 02:06:57 -0500

    Got Meijsters EDT working in 2D.

--- src/DistanceTransform/Meijster2D.hs
+++ src/DistanceTransform/Meijster2D.hs
@@ -1,83 +1,119 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, RecordWildCards #-}
 module DistanceTransform.Meijster2D (edt) where
 import Control.Applicative
-import Control.Monad (when)
+import Control.Monad (when, foldM)
+import Control.Monad.ST (ST)
 import Data.Vector.Storable (Vector, (!))
 import qualified Data.Vector.Storable as V
 import qualified Data.Vector.Storable.Mutable as VM
 import DistanceTransform.Internal.Extended
 
-data Metric = EDT | MDT | CDT
+import Debug.Trace
 
--- rows :: VM.Storable a => Int -> Vector a -> (Int -> Int -> a)
--- rows stride v r i = v ! (r*stride+1)
-
--- cols :: VM.Storable a => Int -> Vector a -> (Int -> Int -> a)
--- cols stride v c i = v ! (i*stride+c)
-
--- Suppose we represent a column (or a row) as a function Int ->
--- a. Then we can offer an interface to apply functions to rows or
--- columns. Such a function would have type ((Int -> a) -> m ()). But
--- this just provides read only access to an array! What we really
--- care about here is the cursor. To make matters worse, we also want
--- to be able to restrict the range of values we apply our function
--- to.
-
--- Function @f@ will be applied sequentially along each row.
+-- Function @f@ will be applied sequentially along each row using the
+-- given column indices.
 rowsM :: Monad m => Int -> Int -> [Int] -> (Int -> m ()) -> m ()
 rowsM m n cs f = mapM_ aux [0..n-1]
   where aux r = mapM_ f $ map (r*m +) cs -- [r*m..r*m+m-1]
 
--- Function @f@ will be applied sequentially down each column.
+-- Function @f@ will be applied sequentially down each column using
+-- the given row indices.
 colsM :: Monad m => Int -> Int -> [Int] -> (Int -> m ()) -> m ()
 colsM m n rs f = mapM_ aux [0..m-1]
   where aux c = mapM_ f $ map ((c +) . (m*)) rs -- [c,c+m .. m*(n-1)+c]
-
--- But I want to be able to go backwards and forwards!
-
--- rowsM :: (Monad m, VM.Storable a) => 
---          Int -> Int -> Vector a -> (Int -> m ()) -> m ()
--- rowsM m n f = mapM_ aux [0..n-1]
---   where aux r = let offset = r * m
---                 in f -> p ! (offset+c))
-
--- colsM :: (Monad m, VM.Storable a) =>
---          Int -> Int -> Vector a -> ((Int -> a) -> m ()) -> m ()
--- colsM m n p f = mapM_ aux [0..m-1]
---   where aux c = f (\r -> p ! (r*m+c))
-
-intervalApply :: Monad m => 
-                 Int -> (Int -> Bool) -> (Int -> Int) -> (Int -> m ()) -> m ()
-intervalApply start stop step f = go start
-  where go !i | stop i = return ()
-              | otherwise = f i >> go (step i)
-
-fillGen :: Monad m => Int -> Int -> (Int -> Int) -> (Int -> m ()) -> m ()
-fillGen start n step f = go 0
-  where go !i | i == n = return ()
-              | otherwise = f i >> go (step i)
 
 -- Build Meijster's G function that finds nearest neighbors down and
 -- up columns.
 prepareG m n p = V.create $ 
                  do v <- VM.new (m*n)
-                    VM.set (VM.slice 0 m v) (m+n) -- m+n ~ PosInf
                     let vwrite = VM.unsafeWrite v
                         vread = VM.unsafeRead v
                         pullDown i = if p ! i == 0
                                      then vwrite i 0
                                      else vread (i-m) >>= vwrite i . (1+)
-                        pushUp i = do d <- min <$> ((+1) <$> vread i)
-                                               <*> vread (i+m)
-                                      vwrite i d
+                        pushUp i = do below <- vread (i+m)
+                                      curr <- vread i
+                                      when (below < curr)
+                                           (vwrite i $! below + 1)
+                    mapM_ (\i -> vwrite i $ if p!i == 0 then 0 else m+n) [0..m-1]
                     colsM m n [1..n-1] pullDown
-                    colsM m n [n-2..0] pushUp
-                    --fillGen m (m*n-m) (+1) pullDown
+                    colsM m n [n-2,n-3..0] pushUp
                     return v
 
+iterateWhileM :: Monad m => (a -> m (Maybe a)) -> a -> m a
+iterateWhileM f = go 
+  where go !x = f x >>= maybe (return x) go
+
+-- Data structure for holding accessors for temporary arrays used for
+-- each row in phase 2 of the algorithm.
+data Temps m = Temps { swrite  :: Int -> Int -> m ()
+                     , sread   :: Int -> m Int
+                     , twrite  :: Int -> Int -> m ()
+                     , tread   :: Int -> m Int
+                     , fMetric :: Int -> Int -> Int 
+                     , sep     :: Int -> Int -> Int }
+
+-- Initialize temporary arrays and partially applied per-row functions
+-- f and sep.
+prepTemps :: Int -> (Int -> Int) -> ST s (Temps (ST s))
+prepTemps m gsq = do s <- VM.new m
+                     t <- VM.new m
+                     VM.unsafeWrite s 0 0
+                     VM.unsafeWrite t 0 0
+                     return $ Temps (VM.unsafeWrite s) (VM.unsafeRead s)
+                                    (VM.unsafeWrite t) (VM.unsafeRead t)
+                                    f sep
+  where f x i = let d = x - i in d*d + gsq i
+        sep i u = (u*u - i*i + gsq u - gsq i) `quot` 2*(u-i)
+
+traceMsg :: Show a => String -> a -> a
+traceMsg msg x = trace (msg++": "++show x) x
+
+-- Phase 2 forward scan across a row.
+scan3 :: Monad m => Int -> Temps m -> Int -> Int -> m Int
+scan3 m Temps{..} !q0 !u =
+  do q <- iterateWhileM qaux q0
+     if q < 0
+     then swrite 0 u >> return 0
+     else do sq <- sread q
+             let w = 1 + sep sq u
+             if w < m
+             then let !q' = q+1
+                  in do swrite q' u
+                        twrite q' w
+                        return q'
+             else return q
+  where qaux !q 
+          | q < 0 = return Nothing
+          | otherwise = do tq <- tread q
+                           sq <- sread q
+                           return $ if fMetric tq sq > fMetric tq u
+                                    then Just $ q - 1
+                                    else Nothing
+
+-- Phase 2 backward scan across a row.
+scan4 :: Monad m => 
+         Int -> (Int -> Int -> m ()) -> Temps m -> Int -> Int -> m Int
+scan4 rowOffset dtwrite Temps{..} !q !u = 
+  do sq <- sread q
+     dtwrite (rowOffset+u) (fMetric u sq)
+     tq <- tread q
+     if u == tq then return (q-1) else return q
+
+phase2 :: Int -> Int -> (Int -> Int -> Int) -> Vector Int
+phase2 m n gRow = V.create $
+                  do --dt <- VM.new (m*n)
+                     dt <- VM.replicate (m*n) 0
+                     mapM_ (aux (VM.unsafeWrite dt)) [0..n-1]
+                     return dt
+  where aux dt y = do temps <- prepTemps m (gRow y)
+                      q <- foldM (scan3 m temps) 0 [1..m-1]
+                      foldM (scan4 (y*m) dt temps) q [m-1,m-2..0]
+                      return ()
+        
 sedt :: (Ord a, Num a, VM.Storable a) => 
         Int -> Int -> Vector a -> Vector Int
-sedt m n p = undefined
+sedt m n p = phase2 m n gRow
   where gSq = V.map (\x->x*x) $ prepareG m n p
         gRow r = (V.slice (r*m) m gSq !)
 
